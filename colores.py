1d98913bd5f81f71f20e4355255c01da11e0383d
Miguel_Taboada <unmedia@me.com>
2024-04-19 10:52:42 +0200

    modificado unas lineas

--- colores.py
+++ colores.py
@@ -1,1 +1,12 @@
-print("Hola Mundo")
+# Programa que saluda al usuario y calcula el año en que cumplirá 100 años
+
+def saludo_y_calculo():
+    nombre = input("Introduce tu nombre: ")
+    edad = int(input("Introduce tu edad: "))
+    año_actual = 2023
+    año_cien = año_actual - edad + 100
+
+
+
+# Ejecutamos la función
+saludo_y_calculo()
