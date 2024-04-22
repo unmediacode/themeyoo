adac43bc75ded62fcbd075e55c22796519f09420
Miguel_Taboada <unmedia@me.com>
2024-04-22 08:58:41 +0200

    nuevas version

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-// script.js
 const songList = document.getElementById('song-list');
-
-fetch('/songs')
-    .then(response => response.json())
-    .then(songs => {
-        songs.forEach(song => {
-            const listItem = document.createElement('li');
-            listItem.textContent = song.name;
-            listItem.dataset.songId = song.id;
-            songList.appendChild(listItem);
-        });
-    });
+const playButton = document.getElementById('play-button');
+const audioPlayer = document.getElementById('audio-player');
 
 songList.addEventListener('click', event => {
-    const songId = event.target.dataset.songId;
-    fetch(`/play/${songId}`)
-        .then(response => response.text())
-        .then(message => console.log(message));
+	const songName = event.target.dataset.song;
+	audioPlayer.src = songName;
+	playButton.textContent = 'Pausa';
+	audioPlayer.play();
+});
+
+playButton.addEventListener('click', () => {
+	if (audioPlayer.paused) {
+		audioPlayer.play();
+		playButton.textContent = 'Pausa';
+	} else {
+		audioPlayer.pause();
+		playButton.textContent = 'Reproducir';
+	}
 });
